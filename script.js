b650c8632fe4ecb36609e4fd132f1143966824f8
Ryan Yang <Ryanyang99@hotmail.com>
2018-01-21 23:17:24 -0800

    Update

--- script.js
+++ script.js
@@ -1,8 +1,23 @@
 var mainText = document.getElementById("mainText");
 var submitBtn = document.getElementById("submitBtn");
-function submitClick(){
+var linkDisplay = document.getElementById("displayLink");
+
+
+
+function submitClick() {
     var firebaseRef = firebase.database().ref();
     var message = mainText.value;
     firebaseRef.child("URL").set(message);
+    firebaseRef.child("L").set(message);
     document.getElementById('displayLink').innerHTML = message;
+    
 }
+
+/*
+function getUrl() {
+    var firebaseGetRef = firebase.database().ref().child("L");
+    firebaseGetRef.on('value', function (snapshot)) {
+    linkDisplay.innerHTML = snapshot.val();
+    }
+}
+*/
